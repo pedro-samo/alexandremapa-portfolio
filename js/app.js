d79030325737b911896d82f13432f03fae4b72b3
Pedro <pedro.s.morais@gmail.com>
2025-03-24 12:42:49 -0300

    feat: news actions films

--- js/app.js
+++ js/app.js
@@ -1,71 +1,65 @@
-    //SCROLL
+const scrollToPage = () => {
+  const menuItens = document.querySelectorAll('.header__nav a[href^="#"')
 
-    const menuItens = document.querySelectorAll('.header__nav a[href^="#"')
+  menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+  })
 
-    menuItens.forEach(item => {
-        item.addEventListener('click', scrollToIdOnClick);
-    })
+  function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+  }
 
-    function getScrollTopByHref(element) {
-        const id = element.getAttribute('href');
-        return document.querySelector(id).offsetTop;
-    }
+  function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 85;
+    scrollToPosition(to);
+  }
 
-    function scrollToIdOnClick(event) {
-        event.preventDefault();
-        const to = getScrollTopByHref(event.target) - 85;
-        scrollToPosition(to);
-    }
+  function scrollToPosition(to) {
+    smoothScrollTo(0, to);
+  }
 
-    function scrollToPosition(to) {
-        //código para nativo
-        //window.scroll({
-        //    top: to,
-        //    behavior: "smooth",
-        //});
-        smoothScrollTo(0, to);
+  function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.currentTarget) - 85;
+    scrollToPosition(to);
+  }
 
-    }
+  function smoothScrollTo(endX, endY, duration) {
+    const startX = window.scrollX || window.pageXOffset;
+    const startY = window.scrollY || window.pageYOffset;
+    const distanceX = endX - startX;
+    const distanceY = endY - startY;
+    const startTime = new Date().getTime();
 
-    function scrollToIdOnClick(event) {
-        event.preventDefault();
-        const to = getScrollTopByHref(event.currentTarget) - 85;
-        scrollToPosition(to);
-    }
+    duration = typeof duration !== 'undefined' ? duration : 600;
 
-    menuItems.forEach(item => {
-        item.addEventListener('click', scrollToIdOnClick);
-    });
-
-    // Caso deseje suporte a browsers antigos / que não suportam scroll smooth nativo
-    /**
-     * Smooth scroll animation
-     * @param {int} endX: destination x coordinate
-     * @param {int) endY: destination y coordinate
-     * @param {int} duration: animation duration in ms
-     */
-    function smoothScrollTo(endX, endY, duration) {
-        const startX = window.scrollX || window.pageXOffset;
-        const startY = window.scrollY || window.pageYOffset;
-        const distanceX = endX - startX;
-        const distanceY = endY - startY;
-        const startTime = new Date().getTime();
-
-        duration = typeof duration !== 'undefined' ? duration : 600;
-
-        // Easing function
-        const easeInOutQuart = (time, from, distance, duration) => {
-            if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
-            return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
-        };
-
-        const timer = setInterval(() => {
-            const time = new Date().getTime() - startTime;
-            const newX = easeInOutQuart(time, startX, distanceX, duration);
-            const newY = easeInOutQuart(time, startY, distanceY, duration);
-            if (time >= duration) {
-                clearInterval(timer);
-            }
-            window.scroll(newX, newY);
-        }, 1000 / 60); // 60 fps
+    // Easing function
+    const easeInOutQuart = (time, from, distance, duration) => {
+      if ((time /= duration / 2) < 1) return distance / 2 * time * time * time * time + from;
+      return -distance / 2 * ((time -= 2) * time * time * time - 2) + from;
     };
+
+    const timer = setInterval(() => {
+      const time = new Date().getTime() - startTime;
+      const newX = easeInOutQuart(time, startX, distanceX, duration);
+      const newY = easeInOutQuart(time, startY, distanceY, duration);
+      if (time >= duration) {
+        clearInterval(timer);
+      }
+      window.scroll(newX, newY);
+    }, 1000 / 60); // 60 fps
+  };
+}
+
+const setYear = () => {
+  const year = new Date().getFullYear();
+  document.querySelector("footer > section").innerText = `©Alexandre Mapa - ${year}`
+}
+
+window.addEventListener("DOMContentLoaded", function () {
+  scrollToPage();
+  setYear();
+});
+
--- js/form.js
+++ js/form.js
@@ -1,13 +1,11 @@
-window.addEventListener("DOMContentLoaded", function() {
-
-  // get the form elements defined in your form HTML above
-  
+const formBehavior = () => {
   var form = document.getElementById("my-form");
+  
+  if(!form) return;
+
   var button = document.getElementById("my-form-button");
   var status = document.getElementById("my-form-status");
 
-  // Success and Error functions for after the form is submitted
-  
   function success() {
     form.reset();
     button.style = "display: none ";
@@ -18,28 +16,30 @@ window.addEventListener("DOMContentLoaded", function() {
     status.innerHTML = "Oops! Algo deu errado.";
   }
 
-  // handle the form submission event
-
-  form.addEventListener("submit", function(ev) {
+  form.addEventListener("submit", function (ev) {
     ev.preventDefault();
     var data = new FormData(form);
     ajax(form.method, form.action, data, success, error);
   });
+
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+}
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  formBehavior();
 });
 
-// helper function for sending an AJAX request
-
-function ajax(method, url, data, success, error) {
-  var xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.setRequestHeader("Accept", "application/json");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState !== XMLHttpRequest.DONE) return;
-    if (xhr.status === 200) {
-      success(xhr.response, xhr.responseType);
-    } else {
-      error(xhr.status, xhr.response, xhr.responseType);
-    }
-  };
-  xhr.send(data);
-}
